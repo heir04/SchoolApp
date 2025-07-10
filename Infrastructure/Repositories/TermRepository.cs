5437a3d24326a50c77db5fac5baa5508ef57abe4
heir04 <ismailayomide32@gmail.com>
2025-07-10 21:39:43 +0100

    feat: Implement constructor for TermRepository to initialize with ApplicationContext

--- Infrastructure/Repositories/TermRepository.cs
+++ Infrastructure/Repositories/TermRepository.cs
@@ -1,10 +1,14 @@
 using SchoolApp.Application.Abstraction.IRepositories;
 using SchoolApp.Core.Domain.Entities;
+using SchoolApp.Infrastructure.Context;
 
 namespace SchoolApp.Infrastructure.Repositories
 {
     public class TermRepository : BaseRepository<Term>, ITermRepository
     {
+        public TermRepository(ApplicationContext context) 
+        {
+        }
         
     }
 }
